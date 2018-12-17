ac99b30fcb13a64ac28e79784cbdeb4d7603750b
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-17 21:59:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,9 +31,10 @@ def monopoly_with_third_tier
 	  :pieces => 4,
 	  :names => {},
 	  :rent_in_dollars => {
-	    
-	  },
- 
+	    :one_piece_owned => 25,
+	            }
+        	}
+        }
 end
 
 def monopoly_with_fourth_tier
